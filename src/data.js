8efa2b36cd8b6ef1c8864422ff83b57b2c4f5a3b
ahmed hakeem <hakeemkoo@hotmail.com>
2021-07-10 10:04:04 -0400

    fux links

--- src/data.js
+++ src/data.js
@@ -43,31 +43,43 @@ export const featuredPortfolio = [
       id: 1,
       title: "Birdwatcher Quack",
       img: "https://i.ibb.co/dryfH7J/Birdwatcher-Quack.png",
+      live:"https://birdwatcherquack.herokuapp.com/",
+      github:"https://github.com/BirdWatcherQuack/BirdWatcher"
     },
     {
       id: 2,
       title: "Plan Surfing",
       img: "https://i.ibb.co/VHfsf9J/plan-Surfing.png",
+      live:"https://hakeem235.github.io/Plan-surfing/",
+      github:"https://github.com/hakeem235/Plan-surfing"
     },
     {
       id: 3,
       title: "Weather Dashbord",
       img: "https://i.ibb.co/12SMtGD/weather-Dashbord.png",
+      live:"https://hakeem235.github.io/Weather-Dashboard/",
+      github:"https://github.com/hakeem235/Weather-Dashboard"
     },
     {
       id: 4,
       title: "Password Generator",
       img: "https://i.ibb.co/vVb4QVr/Password-Generator.png",
+      live:"https://hakeem235.github.io/Password-Generator/",
+      github:"https://github.com/hakeem235/Password-Generator"
     },
     {
       id: 5,
       title: "Daily Scheduler",
       img: "https://i.ibb.co/6NVXTjf/Daily-Sc.png",
+      live:"https://hakeem235.github.io/Work-Day-Scheduler/",
+      github:"https://github.com/hakeem235/Work-Day-Scheduler"
     },
     {
       id: 6,
       title: "Colorcude",
       img: "https://i.ibb.co/GHcp99Q/Colorcude.jpg",
+      live:"",
+      github:""
     },
   ];
   
